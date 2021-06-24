74d85caf41d5c4b4ce6380d97fde59486740b896
Max-1820 <80045506+Max-1820@users.noreply.github.com>
2021-06-24 19:07:27 +0530

    Add files via upload

---
+++ quiz_game.js
@@ -0,0 +1,9 @@
+
+function login(){
+  player1_name=document.getElementById("player1_name").value;
+  player2_name=document.getElementById("player2_name").value;
+  localStorage.setItem("Name1",player1_name);
+  localStorage.setItem("Name2",player2_name);
+  window.location="game_page.html";
+
+}
