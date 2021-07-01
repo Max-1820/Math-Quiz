0d966c3af05e653108de9e619b30d6a50fe2f3d8
Max-1820 <80045506+Max-1820@users.noreply.github.com>
2021-07-01 17:41:01 +0530

    Update quiz_game.js

--- quiz_game.js
+++ quiz_game.js
@@ -4,6 +4,6 @@ function login(){
   player2_name=document.getElementById("player2_name").value;
   localStorage.setItem("Name1",player1_name);
   localStorage.setItem("Name2",player2_name);
-  window.location="game_page.html";
+  window.location="quiz_game.html";
 
 }
